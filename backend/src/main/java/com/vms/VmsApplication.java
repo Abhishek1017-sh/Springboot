36cd808d5940467d79a35e3263d184f1aa0f9e1d
Aditya <aditya1040@zohomail.in>
2026-05-05 15:28:37 +0530

    Resolved all errors and conflicts

--- backend/src/main/java/com/vms/VmsApplication.java
+++ backend/src/main/java/com/vms/VmsApplication.java
@@ -16,6 +16,7 @@ import java.util.List;
 @SpringBootApplication
 public class VmsApplication {
     public static void main(String[] args) {
+        java.util.TimeZone.setDefault(java.util.TimeZone.getTimeZone("Asia/Kolkata"));
         SpringApplication.run(VmsApplication.class, args);
     }
 
